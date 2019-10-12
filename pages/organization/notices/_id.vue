8e6bc7636ebcfb872dc1c1d3fbf296cd2e3e0ea1
MarkNjunge <mark.kamau@outlook.com>
2019-10-12 18:58:15 +0300

    Add request to select winning bid and reject

--- pages/organization/notices/_id.vue
+++ pages/organization/notices/_id.vue
@@ -6,7 +6,25 @@
     <br />
     <h1>BIDS</h1>
     <br />
-    {{bids}}
+    <div class v-for="(bid, index) in bids" :key="bid.bidId">
+      <p>{{index + 1}}: {{bid}}</p>
+      <button @click="selectWinningBid(bid.bidId)">Select as winner</button>
+      <br />
+      <select name="rejectionReason" id="rejectionReason" v-model="rejectionReason">
+        <option value="UNRESPONSIVE">Unresponsive (Does not meet requirements for tender)</option>
+        <option value="OTHER">Other</option>
+      </select>
+      <div class>
+        <label for="rejectionNarrative">Rejection explanation</label>
+        <input
+          type="text"
+          name="rejectionNarrative"
+          id="rejectionNarrative"
+          v-model="rejectionNarrative"
+        />
+      </div>
+      <button @click="rejectBid(bid.bidId)">Reject</button>
+    </div>
   </div>
 </template>
 
@@ -15,29 +33,67 @@ export default {
   data() {
     return {
       notice: null,
-      bids: null
+      bids: null,
+      tenderId: null,
+      rejectionReason: null,
+      rejectionNarrative: null
     };
   },
   async created() {
-    const tenderId = this.$route.params.id;
+    this.tenderId = this.$route.params.id;
 
     try {
       let res = await this.$axios({
         method: "get",
-        url: `/api/notices/${tenderId}`
+        url: `/api/notices/${this.tenderId}`
       });
 
       this.notice = res.data;
 
       res = await this.$axios({
         method: "get",
-        url: `/api/bids?noticeId=${tenderId}`
+        url: `/api/bids?noticeId=${this.tenderId}`
       });
 
       this.bids = res.data;
     } catch (e) {
       console.log(e);
       alert(e.message);
+    }
+  },
+  methods: {
+    async selectWinningBid(bidId) {
+      try {
+        const res = await this.$axios({
+          method: "post",
+          url: `/api/notices/${this.tenderId}/result`,
+          data: {
+            winningBid: bidId
+          }
+        });
+
+        alert("The bid has been selected as the winner.");
+      } catch (e) {
+        console.log(e);
+        alert(e.response.data.message || e.message);
+      }
+    },
+    async rejectBid(bidId) {
+      try {
+        const res = await this.$axios({
+          method: "post",
+          url: `/api/bids/${encodeURIComponent(bidId)}/reject`,
+          data: {
+            reason: this.rejectionReason,
+            reasonNarrative: this.rejectionNarrative
+          }
+        });
+
+        alert("The bid has been rejected.");
+      } catch (e) {
+        console.log(e);
+        alert(e.response.data.message || e.message);
+      }
     }
   }
 };
