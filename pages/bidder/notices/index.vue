ed1e46d01d41b3344c73df741aa8ba2933f6c873
MarkNjunge <mark.kamau@outlook.com>
2019-10-15 14:32:54 +0300

    Add download for TenderNotice

--- pages/bidder/notices/index.vue
+++ pages/bidder/notices/index.vue
@@ -2,7 +2,11 @@
   <div>
     <h1>ALL NOTICES</h1>
     <br />
-    {{notices}}
+    <div v-for="(notice) in notices" :key="notice.tenderId">
+      {{notice}}
+      <button @click="download(notice.tenderDocument.documentRef)">Download</button>
+      <br />
+    </div>
   </div>
 </template>
 
@@ -26,6 +30,42 @@ export default {
     } catch (e) {
       console.log(e);
       alert(e.message);
+    }
+  },
+  methods: {
+    async download(documentRef) {
+      try {
+        const res = await this.$axios({
+          method: "get",
+          url: `/api/files/documents?ref=${encodeURIComponent(documentRef)}`
+        });
+
+        window.location = `http://localhost:3000/files/documents?ref=${encodeURIComponent(
+          documentRef
+        )}`;
+
+        // Try to find out the filename from the content disposition `filename` value
+        //  =======================================
+        // const disposition = res.headers["content-disposition"];
+        // const matches = /"([^"]*)"/.exec(disposition);
+        // const filename = matches != null && matches[1] ? matches[1] : "file.pdf";
+
+        // // The actual download
+        // const blob = new Blob([res.data], { type: "application/pdf" });
+        // const link = document.createElement("a");
+        // link.href = window.URL.createObjectURL(blob);
+        // link.download = filename;
+
+        // document.body.appendChild(link);
+
+        // link.click();
+
+        // document.body.removeChild(link);
+        //  =======================================
+      } catch (e) {
+        console.log(e);
+        alert(e.message);
+      }
     }
   }
 };
