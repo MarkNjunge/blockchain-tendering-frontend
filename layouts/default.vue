7b83e8bf510c764992c769ee81501d21da51a28a
MarkNjunge <mark.kamau@outlook.com>
2019-10-17 16:08:22 +0300

    Update create notice UI

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,9 +1,29 @@
 <template>
   <div>
-    <nuxt />
+    <div id="page-bg" class="bg-gray-100"></div>
+    <symbols></symbols>
+    <nuxt id="page-content" class="pt-4" />
   </div>
 </template>
 
+<script>
+import Symbols from "../components/Symbols";
+export default {
+  components: { Symbols }
+};
+</script>
+
 <style lang="scss">
 @import "../assets/style/main";
+
+#page-content {
+  position: relative;
+}
+
+#page-bg {
+  position: fixed;
+  width: 100vw;
+  height: 100vh;
+  overflow: hidden;
+}
 </style>
--- pages/organization/notices/create.vue
+++ pages/organization/notices/create.vue
@@ -1,56 +1,120 @@
 <template>
-  <div>
-    <h1>Create notice</h1>
-    <form @submit="submit">
-      <div class>
-        <label for="ref">Reference Number</label>
-        <input type="text" id="ref" name="ref" v-model="ref" required />
-      </div>
-      <div class>
-        <label for="title">Title</label>
-        <input type="text" id="title" name="title" v-model="title" required />
-      </div>
-      <div class>
-        <label for="tenderDocument">Tender Document</label>
-        <input type="file" name="card" id="card" @change="onFileSelected" />
-      </div>
-      <div class>
-        <label for="requiredDocuments">Required Documents</label>
-        <input
-          type="text"
-          id="requiredDocuments"
-          name="requiredDocuments"
-          placeholder="Enter a list separated by commas"
-          v-model="requiredDocuments"
-          required
+  <div class="container mx-auto px-4 xl:w-1/2 md:w-4/5">
+    <nuxt-link to="/organization" class="up-navigation">
+      <!-- M1 -->
+      <svg viewBox="0 0 320 512" class="up-navigation-icon">
+        <path
+          fill="currentColor"
+          d="M34.52 239.03L228.87 44.69c9.37-9.37 24.57-9.37 33.94 0l22.67 22.67c9.36 9.36 9.37 24.52.04 33.9L131.49 256l154.02 154.75c9.34 9.38 9.32 24.54-.04 33.9l-22.67 22.67c-9.37 9.37-24.57 9.37-33.94 0L34.52 272.97c-9.37-9.37-9.37-24.57 0-33.94z"
         />
-      </div>
-      <div class>
-        <label for="closingDate">Closing Date</label>
-        <input
-          type="datetime-local"
-          name="closingDate"
-          id="closingDate "
-          v-model="closingDate"
-          required
-        />
-      </div>
-      <div class>
-        <label for="openingVenue">Opening Venue</label>
-        <input type="text" name="openingVenue" id="openingVenue" v-model="openingVenue" required />
-      </div>
-      <div class>
-        <label for="openingDate">Opening Date</label>
-        <input
-          type="datetime-local"
-          name="openingDate"
-          id="openingDate"
-          v-model="openingDate"
-          required
-        />
-      </div>
-      <button type="submit">Publish</button>
-    </form>
+      </svg>
+      <!-- M2 -->
+      <!-- <svg class="up-navigation-icon">
+        <use href="#back" />
+      </svg>-->
+      <p class="up-navigation-text">BACK</p>
+    </nuxt-link>
+
+    <h1 class="section-title mt-2">Create a Tender Notice</h1>
+
+    <div class="mt-2 mb-8 card">
+      <form @submit="submit">
+        <div class="input-group">
+          <label for="ref" class="input-label">Reference Number</label>
+          <input type="text" id="ref" name="ref" class="input-field" v-model="ref" required />
+        </div>
+        <div class="mt-3 input-group">
+          <label for="title" class="input-label">Title</label>
+          <input type="text" id="title" name="title" class="input-field" v-model="title" required />
+        </div>
+        <div class="mt-3 input-group">
+          <label for="tenderDocument" class="input-label">Tender Document</label>
+          <input type="file" name="card" id="card" class="input-field" @change="onFileSelected" />
+        </div>
+        <div class="mt-3 input-group">
+          <label for="requiredDocuments" class="input-label">Required Documents</label>
+          <div class="input-checkbox">
+            <input
+              type="checkbox"
+              name="nhif"
+              id="nhif"
+              class="input-checkbox-box"
+              value="nhif"
+              v-model="nhif"
+            />
+            <label for="nhif" class="input-checkbox-label">NHIF Clearance Certificate</label>
+          </div>
+          <div class="input-checkbox">
+            <input
+              type="checkbox"
+              name="kra"
+              id="kra"
+              class="input-checkbox-box"
+              value="kra"
+              v-model="kra"
+            />
+            <label for="kra" class="input-checkbox-label">KRA Clearance Certificate</label>
+          </div>
+          <div class="input-checkbox">
+            <input
+              type="checkbox"
+              name="bidBond"
+              id="bidBond"
+              value="BidBond"
+              v-model="bidBond"
+              class="input-checkbox-box"
+            />
+            <label for="bidBond" class="input-checkbox-label">Bid Bond</label>
+          </div>
+        </div>
+        <div class="mt-3 input-group">
+          <label for="extraRequiredDocuments" class="input-label">Extra Required Documents</label>
+          <input
+            type="text"
+            id="extraRequiredDocuments"
+            name="extraRequiredDocuments"
+            placeholder="Enter a list separated by commas"
+            class="input-field"
+            v-model="extraRequiredDocuments"
+          />
+        </div>
+        <div class="mt-3 input-group">
+          <label for="closingDate" class="input-label">Tender Closing Date</label>
+          <input
+            type="datetime-local"
+            name="closingDate"
+            id="closingDate"
+            class="input-field"
+            v-model="closingDate"
+            @change="onClosingDateChange"
+            required
+          />
+        </div>
+        <div class="mt-3 input-group">
+          <label for="openingDate" class="input-label">Tender Opening Date</label>
+          <input
+            type="datetime-local"
+            name="openingDate"
+            id="openingDate"
+            class="input-field"
+            v-model="openingDate"
+            required
+          />
+        </div>
+        <div class="mt-3 input-group">
+          <label for="openingVenue" class="input-label">Tender Opening Venue</label>
+          <input
+            type="text"
+            name="openingVenue"
+            id="openingVenue"
+            class="input-field"
+            v-model="openingVenue"
+            required
+          />
+        </div>
+        <button type="submit" class="mt-6 w-full btn">Publish</button>
+      </form>
+    </div>
   </div>
 </template>
 
@@ -61,7 +125,10 @@ export default {
       openingDate: null,
       openingVenue: null,
       closingDate: null,
-      requiredDocuments: null,
+      nhif: true,
+      kra: true,
+      bidBond: false,
+      extraRequiredDocuments: null,
       title: null,
       ref: null
     };
@@ -70,6 +137,9 @@ export default {
     onFileSelected(event) {
       this.tenderDocument = event.target.files[0];
     },
+    onClosingDateChange() {
+      this.openingDate = this.closingDate;
+    },
     async submit(e) {
       e.preventDefault();
 
@@ -77,14 +147,25 @@ export default {
         return;
       }
 
+      const requiredDocuments = this.extraRequiredDocuments
+        .split(",")
+        .map(v => v.trim());
+
+      if (this.nhif) {
+        requiredDocuments.unshift("nhif");
+      }
+      if (this.kra) {
+        requiredDocuments.unshift("kra");
+      }
+      if (this.bidBond) {
+        requiredDocuments.unshift("bid bond");
+      }
+
       const bodyFormData = new FormData();
       bodyFormData.append("id", this.ref);
       bodyFormData.append("document", this.tenderDocument);
       bodyFormData.append("title", this.title);
-      bodyFormData.append(
-        "requiredDocuments",
-        JSON.stringify(this.requiredDocuments.split(",").map(v => v.trim()))
-      );
+      bodyFormData.append("requiredDocuments", JSON.stringify({}));
       bodyFormData.append("closingDate", this.closingDate);
       bodyFormData.append("openingVenue", this.openingVenue);
       bodyFormData.append("openingDate", this.openingDate);
