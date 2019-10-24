8c4db87e29851b2f4c388eeeb53c54d4ef1d8d24
MarkNjunge <mark.kamau@outlook.com>
2019-10-24 17:19:48 +0300

    Add loading indicator

---
+++ components/Loading.vue
@@ -0,0 +1,31 @@
+<template>
+  <transition name="fade">
+    <div
+      class="mt-4 w-full h-full flex flex-col items-center justify-center"
+      v-if="visible == true"
+    >
+      <svg class="w-16 h-16 fill-current text-blue-700">
+        <use href="#loader" />
+      </svg>
+      <p class="mt-4 text-xl text-blue-800">Loading...</p>
+    </div>
+  </transition>
+</template>
+
+<script>
+export default {
+  name: "loading",
+  props: ["visible"]
+};
+</script>
+
+<style lang="css">
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.5s;
+}
+.fade-enter,
+.fade-leave-to {
+  opacity: 0;
+}
+</style>
--- pages/bidder/notices/index.vue
+++ pages/bidder/notices/index.vue
@@ -1,46 +1,51 @@
 <template>
   <div class="container mx-auto">
-    <h2 class="mt-8 section-title">Available Tenders</h2>
-    <div class="mt-2 notices">
-      <table>
-        <thead>
-          <tr>
-            <th>Notice ID</th>
-            <th>Title</th>
-            <th>Organization</th>
-            <th>Required document(s)</th>
-            <th>Deadline</th>
-            <th>Status</th>
-            <th></th>
-            <!-- <th>Opening Venue</th> -->
-          </tr>
-        </thead>
-        <tbody>
-          <tr v-for="(notice) in notices" :key="notice.tenderId">
-            <td>{{notice.tenderId}}</td>
-            <td>{{notice.title}}</td>
-            <td>{{notice.organization.name}}</td>
-            <td>{{notice.requiredDocuments}}</td>
-            <td>{{notice.submissionClosingDate}}</td>
-            <td>
-              <p class="tag bg-gray-200 text-gray-800">{{notice.status}}</p>
-            </td>
-            <td>
-              <svg
-                class="w-4 h-4 fill-current cursor-pointer hover:text-blue-700"
-                @click="openNotice(notice)"
-              >
-                <use href="#open-link" />
-              </svg>
-            </td>
-          </tr>
-        </tbody>
-      </table>
+    <loading :visible="loading" />
+    <div v-if="notices">
+      <h2 class="mt-8 section-title">Available Tenders</h2>
+      <div class="mt-2 notices">
+        <table>
+          <thead>
+            <tr>
+              <th>Notice ID</th>
+              <th>Title</th>
+              <th>Organization</th>
+              <th>Required document(s)</th>
+              <th>Deadline</th>
+              <th>Status</th>
+              <th></th>
+              <!-- <th>Opening Venue</th> -->
+            </tr>
+          </thead>
+          <tbody>
+            <tr v-for="(notice) in notices" :key="notice.tenderId">
+              <td>{{notice.tenderId}}</td>
+              <td>{{notice.title}}</td>
+              <td>{{notice.organization.name}}</td>
+              <td>{{notice.requiredDocuments}}</td>
+              <td>{{notice.submissionClosingDate}}</td>
+              <td>
+                <p class="tag bg-gray-200 text-gray-800">{{notice.status}}</p>
+              </td>
+              <td>
+                <svg
+                  class="w-4 h-4 fill-current cursor-pointer hover:text-blue-700"
+                  @click="openNotice(notice)"
+                >
+                  <use href="#open-link" />
+                </svg>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
+import Loading from "@/components/Loading";
+
 String.prototype.capitalize = function() {
   return this.replace(/(?:^|\s)\S/g, function(a) {
     return a.toUpperCase();
@@ -48,18 +53,25 @@ String.prototype.capitalize = function() {
 };
 
 export default {
+  components: {
+    Loading
+  },
   data() {
     return {
-      notices: null
+      notices: null,
+      loading: false
     };
   },
   async mounted() {
+    this.loading = true;
+
     try {
       const res = await this.$axios({
         method: "get",
         url: `/api/notices`
       });
 
+      this.loading = false;
       this.notices = res.data.map(n => {
         n.requiredDocuments = n.requiredDocuments
           .map(d =>
@@ -72,6 +84,7 @@ export default {
         return { ...n };
       });
     } catch (e) {
+      this.loading = false;
       console.log(e);
       alert(e.message);
     }
