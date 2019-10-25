4970d24deb0c2073ba0d7e2eb75fed6884e50d0b
MarkNjunge <mark.kamau@outlook.com>
2019-10-25 12:03:59 +0300

    Update bidder dashboard

---
+++ components/BidCard.vue
@@ -0,0 +1,31 @@
+<template>
+  <nuxt-link
+    :to="'/bidder/notices/' + encodeURIComponent(bid.tenderNotice.tenderId)"
+    class="p-6 bg-white rounded border border-gray-400 shadow cursor-pointer"
+  >
+    <div class="flex justify-between items-baseline">
+      <h2 class="text-xl">{{bid.bidId}}</h2>
+      <p class="tag bg-gray-200 text-gray-800">{{bid.status}}</p>
+    </div>
+    <p class="text-gray-700">{{bid.tenderNotice.tenderId}}</p>
+    <div class="mt-4 flex justify-between items-baseline">
+      <p class="text-gray-500">{{bid.datePosted}}</p>
+      <p
+        class="py-1 px-2 font-bold rounded bg-red-200 text-red-800 hover:bg-red-600 hover:text-black"
+        @click="withdraw(bid)"
+      >Withdraw</p>
+    </div>
+  </nuxt-link>
+</template>
+
+<script>
+export default {
+  name: "BidCard",
+  props: ["bid"],
+  methods: {
+    withdraw(bid) {
+      this.$parent.withdraw(bid);
+    }
+  }
+};
+</script>
--- pages/bidder/index.vue
+++ pages/bidder/index.vue
@@ -1,29 +1,67 @@
 <template>
   <div>
-    <h1>Dashboard</h1>
-    <nuxt-link to="/profile">Profile</nuxt-link>
-    <p @click="logout">Logout</p>
-    <p v-if="participantType">Logged in as {{participantType}}</p>
+    <div v-if="bids" class="container mx-auto mt-8">
+      <h2 class="text-gray-700">Recent bids</h2>
+      <div class="recent-bids mt-2 mb-2">
+        <BidCard v-for="(bid, index) in bids" :key="index" :bid="bid" />
+      </div>
+      <nuxt-link to="/bidder/bids" class="font-bold hover:text-blue-700">See all</nuxt-link>
+    </div>
+    <div class="container mx-auto mt-6" v-if="notices">
+      <h2 class="text-gray-700">Recent Notices</h2>
+      <div class="recent-notices mt-2 mb-2">
+        <BidderNoticeCard v-for="(notice, index) in notices" :key="index" :notice="notice" />
+      </div>
+      <nuxt-link to="/bidder/notices" class="font-bold hover:text-blue-700">See all</nuxt-link>
+    </div>
+    <Loading :visible="loading" />
   </div>
 </template>
 
 <script>
+import BidCard from "@/components/BidCard";
+import BidderNoticeCard from "@/components/BidderNoticeCard";
+import Loading from "@/components/Loading";
+
 export default {
+  components: {
+    BidCard,
+    Loading,
+    BidderNoticeCard
+  },
   data() {
     return {
-      participantType: null
+      loading: false,
+      bids: null,
+      notices: null
     };
   },
-  mounted() {
-    let session = window.localStorage.getItem("session");
-    if (!session) {
-      console.log("Not logged in");
-      this.$nuxt.$router.replace({ path: "/login" });
-      return;
-    }
-    session = JSON.parse(session);
+  async created() {
+    this.loading = true;
+    let res = await this.$axios({
+      method: "GET",
+      url: "/api/bids/forCurrent"
+    });
 
-    this.participantType = session.participantType;
+    this.bids = res.data.slice(0, 3).map(n => {
+      const d = new Date(n.datePosted);
+      const options = { dateStyle: "long" };
+      n.datePosted = d.toLocaleDateString("en-US", options);
+      return { ...n };
+    });
+
+    res = await this.$axios({
+      method: "GET",
+      url: "/api/notices"
+    });
+    this.notices = res.data.slice(0, 3).map(n => {
+      const d = new Date(n.submissionClosingDate);
+      const options = { dateStyle: "long" };
+      n.submissionClosingDate = d.toLocaleDateString("en-US", options);
+      return { ...n };
+    });
+
+    this.loading = false;
   },
   methods: {
     async logout() {
@@ -35,7 +73,19 @@ export default {
       window.localStorage.removeItem("session");
       window.localStorage.removeItem("profile");
       this.$nuxt.$router.replace({ path: "/login" });
+    },
+    async withdraw(bid) {
+      console.log(`Withdraw: ${bid.bidId}`);
     }
   }
 };
 </script>
+
+<style lang="scss">
+.recent-bids,
+.recent-notices {
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  grid-gap: 1rem;
+}
+</style>
